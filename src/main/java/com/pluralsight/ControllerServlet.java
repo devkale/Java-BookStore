23fa0668ed7dae931d7527de0d800375d956a9d4
devkale <kaleaditya92@gmail.com>
2018-12-21 14:06:30 +0530

    Added update in db functionality

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -1,5 +1,7 @@
 package com.pluralsight;
 
+//import com.sun.xml.internal.bind.v2.model.core.ID;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
@@ -65,6 +67,9 @@ public class ControllerServlet extends HttpServlet {
 				case "/edit":
 					showEditForm(request, response);
 			break;
+				case "/update":
+					updateBook(request, response);
+		break;
         default:
 				   listBooks(request, response);
            break;
@@ -127,6 +132,18 @@ public class ControllerServlet extends HttpServlet {
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/BookForm.jsp");
 		request.setAttribute("book", book);
 		requestDispatcher.forward(request, response);
+	}
+
+	private void updateBook(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException
+	{
+		int id = Integer.parseInt( request.getParameter("id"));
+		String title = request.getParameter("booktitle");
+		String author = request.getParameter("bookauthor");
+		Float price = Float.parseFloat( request.getParameter("bookprice") );
+		Book book = new Book(id, title, author, price);
+		bookDAO.updateBook(book);
+		response.sendRedirect("list");
 	}
 
 	/**
